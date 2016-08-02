4e9ae5e3ef4386a6d0e3aedf924864e42145f3c0
Qian Li <liqian.cs@gmail.com>
2016-08-02 16:28:16 -0400

    update gitignore

--- run_gather.sh
+++ run_gather.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-build/X86/gem5.opt configs/example/se.py --cpu-type=timing -c ../gather_test --options="1"
+build/X86/gem5.opt configs/example/se.py --cpu-type=timing -c ../gather_test --options="6"
 
